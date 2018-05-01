0253328cec1f0831c199de96291466fbf9f3c6d0
Anthony Monthe <anthony.monthe@gmail.com>
2018-05-02 00:40:27 +0100

    Improved datacenter ID

--- ec2_sizes/__init__.py
+++ ec2_sizes/__init__.py
@@ -1,5 +1,5 @@
 """EC2 instance types as constants."""
-VERSION = (0, 4)
+VERSION = (0, 5)
 __version__ = '.'.join([str(i) for i in VERSION])
 __author__ = 'Anthony Monthe (ZuluPro)'
 __email__ = 'anthony.monthe@gmail.com'
--- ec2_sizes/scrap.py
+++ ec2_sizes/scrap.py
@@ -7,6 +7,29 @@ import ijson
 FILEPATH = '/tmp/ec.json'
 URL = "https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/AmazonEC2/current/index.json"
 IGNORED_FIELDS = ['locationType', 'operatingSystem']
+REGION_NAME_MATCH = {
+    # America
+    'US East (Ohio)': 'us-east-2',
+    'US West (N. California)': 'us-west-1',
+    'US West (Oregon)': 'us-west-2',
+    'US East (N. Virginia)': 'us-east-1',
+    'Canada (Central)': 'ca-central-1',
+    'South America (Sao Paulo)': 'sa-east-1',
+    'AWS GovCloud (US)': 'us-gov-west-1',
+    # EU
+    'eu-west-1': 'eu-west-1',
+    'EU (Ireland)': 'eu-west-1',
+    'EU (London)': 'eu-west-2',
+    'EU (Paris)': 'eu-west-3',
+    'EU (Frankfurt)': 'eu-central-1',
+    # Asia
+    'Asia Pacific (Mumbai)': 'ap-south-1',
+    'Asia Pacific (Singapore)': 'ap-southeast-1',
+    'Asia Pacific (Sydney)': 'ap-southeast-2',
+    'Asia Pacific (Tokyo)': 'ap-northeast-1',
+    'Asia Pacific (Seoul)': 'ap-northeast-2',
+    'Asia Pacific (Osaka-Local)': 'ap-northeast-3',
+}
 
 
 def download_json():
@@ -35,8 +58,11 @@ def parse():
         if products_data[sku]['productFamily'] != "Compute Instance":
             continue
         location = products_data[sku]['attributes']['location']
+        if location not in REGION_NAME_MATCH:
+            continue
+        az = REGION_NAME_MATCH[location]
         instance_type = products_data[sku]['attributes']['instanceType']
-        regions[location].append(instance_type)
+        regions[az].append(instance_type)
         if instance_type not in flavors:
             for field in IGNORED_FIELDS:
                 products_data[sku]['attributes'].pop(field, None)
@@ -46,15 +72,15 @@ def parse():
 
 def dump():
     flavors, regions = parse()
+    with open('ec2-sizes.pkl', 'wb') as fd:
+        pickle.dump(flavors, fd, protocol=2)
+    with open('ec2-regions.pkl', 'wb') as fd:
+        pickle.dump(regions, fd, protocol=2)
     flavors, regions = json.dumps(flavors), json.dumps(regions)
     with open('ec2-sizes.json', 'w') as fd:
         fd.write(flavors)
-    with open('ec2-sizes.pkl', 'wb') as fd:
-        pickle.dump(flavors, fd, protocol=2)
     with open('ec2-regions.json', 'w') as fd:
         fd.write(regions)
-    with open('ec2-regions.pkl', 'wb') as fd:
-        pickle.dump(regions, fd, protocol=2)
 
 
 if __name__ == '__main__':
